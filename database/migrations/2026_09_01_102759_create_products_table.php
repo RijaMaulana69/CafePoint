e0db9d9152054300dcdce887738b5b20fdb58cd0
RizaMaulana0_ <ghineza31@gmail.com>
2026-09-04 15:33:02 +0700

    menambahkan fitur transaksi

--- database/migrations/2026_09_01_102759_create_products_table.php
+++ database/migrations/2026_09_01_102759_create_products_table.php
@@ -14,21 +14,16 @@ return new class extends Migration
         Schema::create('products', function(Blueprint $table)
         {
             $table->id();
-
             $table->foreignId('category_id')
                 ->constrained('categories')
                 ->cascadeOnDelete();
-
             $table->string('code', 50)->unique();
             $table->string('name', 150);
             $table->text('description')->nullable();
-
             $table->decimal('purchase_price', 15, 2)->default(0);
             $table->decimal('selling_price', 15, 2)->default(0);
-            
             $table->integer('stock')->default(0);
             $table->integer('minimum_stock')->default(0);
-
             $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
